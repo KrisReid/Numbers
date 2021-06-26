ed389958a3b1b312ee3f51921ee655631fba4ce9
Kris Reid <krisreid_06@hotmail.com>
2021-06-26 19:54:00 +0100

    Updates to the view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var contentVM = ContentViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
+
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                
+                Text("Number - Text")
+                    .font(.system(size: 28, weight: .bold))
+                    .foregroundColor(Color.white)
+                    .padding(.vertical, 20)
+                
+                VStack(alignment: .leading) {
+                    
+                    Text("Type in a number from 0 - 999")
+                        .font(.system(size: 15, weight: .light))
+                        .padding(.bottom, 6)
+                    
+                    HStack {
+                        Spacer()
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        TextField("Search", text: Binding(
+                            get: { String(contentVM.number) },
+                            set: { contentVM.number = Int($0) ?? 0 }
+                        ))
+                        .keyboardType(.numberPad)
+                        .foregroundColor(Color.gray)
+                        .padding(10)
+                        
+                        Spacer()
+                    }
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10)
+                    
+                    HStack {
+                        Spacer()
+                        Text("\(contentVM.number)")
+                            .padding(.top, 2)
+                            .font(.system(size: 14, weight: .light))
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Button(action: {
+                                contentVM.numberConverter()
+                            }, label: {
+                                Text("Convert to text")
+                            })
+                            .padding(.horizontal, 30)
+                            .padding(.vertical, 10)
+                            .font(.system(size: 14, weight: .light))
+                            .background(Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)))
+                            .cornerRadius(10)
+                            
+                            Text("\(contentVM.text)")
+                                .padding()
+                                .font(.system(size: 14, weight: .light))
+                        }
+                        Spacer()
+                    }
+                }
+                .font(.system(size: 14, weight: .semibold))
+                .foregroundColor(.white)
+                .padding()
+                .background(Color(.init(white: 1, alpha: 0.3)))
+                .cornerRadius(10)
+                
+                Spacer()
+            }
             .padding()
+        }
     }
 }
 
--- Shared/ContentViewModel.swift
+++ Shared/ContentViewModel.swift
@@ -6,3 +6,88 @@
 //
 
 import Foundation
+import SwiftUI
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var number: Int = 0
+    @Published var text: String = ""
+    
+    func numberConverter() {
+        
+        //Create some dictionaries of the raw data i'll need
+        let singleDigits: [Int: String] = [0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"]
+        let teenDigits: [Int: String] = [0: "Ten", 1: "Eleven", 2: "Twelve", 3: "Thirteen", 4: "Fourteen", 5: "Fifteen", 6: "Sixteen", 7: "Seventeen", 8: "Eighteen", 9: "Nineteen"]
+        let noughtyDigits: [Int: String] = [0: "", 1: "Teens", 2: "Twenty", 3: "Thirty", 4: "Fourty", 5: "Fifty", 6: "Sixty", 7: "Seventy", 8: "Eighty", 9: "Ninety"]
+        
+        //Break the number into an array of Integers
+        let stringDigits = String(number).compactMap{ $0.wholeNumberValue }
+        
+        //How many digits are in the number - needed to apply logic?
+        let count = String(number).count
+        
+        //Use the count of digits to help determine the rules in a switch statement
+        switch count {
+        
+        // 1 digit (i.e. 0-9)
+        case 1:
+            SingleDigits(singleDigits: singleDigits, stringDigits: stringDigits)
+           
+        // 2 digits (i.e. 10 - 99)
+        case 2:
+            text = DoubleDigits(singleDigits: singleDigits, teenDigits: teenDigits, noughtyDigits: noughtyDigits, stringDigits: stringDigits, hundreds: false)
+
+        // 3 digits (i.e. 100 - 999)
+        case 3:
+            trebleDigits(singleDigits: singleDigits, teenDigits: teenDigits, noughtyDigits: noughtyDigits, stringDigits: stringDigits, hundreds: true)
+
+        default:
+            text = "Number is too big 🤯"
+        }
+    }
+    
+    
+    private func SingleDigits(singleDigits: [Int: String], stringDigits: [Int]) {
+        let matchedNumber = singleDigits.filter {$0.key == stringDigits[0]}
+        text = matchedNumber.values.first ?? "Error"
+    }
+    
+    
+    func DoubleDigits(singleDigits: [Int: String], teenDigits: [Int: String], noughtyDigits: [Int: String], stringDigits: [Int], hundreds: Bool) -> String {
+        
+        let firstNumber = noughtyDigits.filter {$0.key == stringDigits[hundreds ? 1 : 0]}
+        let teenNumber = teenDigits.filter {$0.key == stringDigits[hundreds ? 2 : 1]}
+        let secondNumber = singleDigits.filter {$0.key == stringDigits[hundreds ? 2 : 1]}
+        
+        //sort out the teens (if the first number is 1 then reference the matchedTeenNumber (which looks at the second digit to determine the value))
+        if (firstNumber == [1: "Teens"]) {
+            return "\(teenNumber.values.first ?? "Error")"
+        } else {
+            //Sort out the 'noughties' (If the first number is 0 then only reference the matchedFirstNumber, else reference both matchedFirstNumber and matchedSecondNumber)
+            if (secondNumber == [0: "Zero"]) {
+                return "\(firstNumber.values.first ?? "Error")"
+            } else {
+                return "\(firstNumber.values.first ?? "Error") \(secondNumber.values.first ?? "Error")"
+            }
+        }
+    }
+    
+    
+    private func trebleDigits (singleDigits: [Int: String], teenDigits: [Int: String], noughtyDigits: [Int: String], stringDigits: [Int], hundreds: Bool) {
+        
+        let firstNumber = singleDigits.filter {$0.key == stringDigits[0]}
+        let secondNumber = singleDigits.filter {$0.key == stringDigits[1]}
+        let thirdNumber = singleDigits.filter {$0.key == stringDigits[2]}
+        
+        //sort out the round hundreds
+        if secondNumber == [0: "Zero"] && thirdNumber == [0: "Zero"] {
+            text = "\(firstNumber.values.first ?? "Error") hundred"
+        } else {
+            let lastTwoDigits = DoubleDigits(singleDigits: singleDigits, teenDigits: teenDigits, noughtyDigits: noughtyDigits, stringDigits: stringDigits, hundreds: hundreds)
+            
+            //Take the last to 2 numbers and pass them to the DoubleDigits function and get a returned response to display
+            text = "\(firstNumber.values.first ?? "Error") hundred and \(lastTwoDigits)"
+        }
+    }
+
+}
