7579a6b670b9fb729859de864e39d37bbc1f86a4
Kris Reid <krisreid_06@hotmail.com>
2021-06-26 13:22:28 +0100

    Initial Commit

---
+++ Shared/NumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumbersApp.swift
+//  Shared
+//
+//  Created by Kris Reid on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct NumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
